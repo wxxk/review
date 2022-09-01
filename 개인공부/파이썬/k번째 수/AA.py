f095f43dcde487bdb527ad9443ae877d23bf3cf8
wxxk <dwde22@gmail.com>
2022-09-02 00:04:21 +0900

    개인공부

---
+++ 개인공부/파이썬/k번째 수/AA.py
@@ -0,0 +1,13 @@
+import sys
+# sys.stdin = open('input.txt')
+
+T = int(input())
+
+for t in range(T):
+    n, s, e, k = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    numbers=numbers[s-1:e]
+    numbers.sort()
+    print("#%d %d" %(t+1, numbers[k-1]))
+
+    
